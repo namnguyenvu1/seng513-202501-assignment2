8c843d9daaa8591e1e1d17ac0bcaa682f77ca038
Hera-Naeem <hera.naeem@ucalgary.ca>
2025-03-10 21:25:51 -0600

    increased number of fetched questions to 10

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 // This file will handle fetching questions asynchronously from the website on the ass description
 export async function fetchQuestions(difficulty = 'easy') {
-    // 5 question for now, alter if you guys need
-    const url = `https://opentdb.com/api.php?amount=5&type=multiple&difficulty=${difficulty}`; 
+    // 10 question for now, alter if you guys need
+    const url = `https://opentdb.com/api.php?amount=10&type=multiple&difficulty=${difficulty}`; 
     const response = await fetch(url);
     const data = await response.json();
     return data.results;
